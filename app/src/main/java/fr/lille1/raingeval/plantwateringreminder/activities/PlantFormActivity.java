79f5caaadeff36314b324efb8ee31f6412f94622
Anthony Raingeval <antcho60@gmail.com>
2016-11-28 16:43:09 +0100

    Fix back button behavior on PlantFormActivity

--- app/src/main/java/fr/lille1/raingeval/plantwateringreminder/activities/PlantFormActivity.java
+++ app/src/main/java/fr/lille1/raingeval/plantwateringreminder/activities/PlantFormActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -212,5 +213,17 @@ public class PlantFormActivity extends AppCompatActivity {
         Plant newPlant = new Plant(null, name, frequency, 0, MainActivity.currentTime);
         return newPlant;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
